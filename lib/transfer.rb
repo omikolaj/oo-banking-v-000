63da59efeed836f36c6853dbbd348987a2e63488
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-16 10:30:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,9 +26,11 @@ class Transfer
   end
 
   def reverse_transfer
-    self.sender.balance += self.amount
-    self.receiver.balance -= self.amount
-    self.status = "reversed"
+    if(self.status == "complete")
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
   end
 
 end
