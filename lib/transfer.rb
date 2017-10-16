0df91abef71d6f69f58b1e15158c18ad05df9d5a
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-16 10:09:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,10 @@
 class Transfer
   # your code here
-  attr_accessor :transferee, :transferer, :amount
+  attr_accessor :transferee, :sender, :amount
 
-  def initialize(transferer, transferee, amount)
+  def initialize(transferer, sender, amount)
     self.transferee = transferee
-    self.transferer = transferer
+    self.sender = sender
     self.amount = amount
   end
 end
