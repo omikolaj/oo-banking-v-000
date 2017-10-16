1936d8a5ed779e8c0aacc2afb82e27686ee4716d
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-16 10:25:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,7 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if(!self.valid?)
       "Transaction rejected. Please check your account balance."
     elsif(self.status == "pending")
