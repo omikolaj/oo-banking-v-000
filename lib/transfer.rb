46094fc1c9680b30df0429a4c7fa545057df7132
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-16 10:08:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
   # your code here
+  attr_accessor :transferee, :transferer, :amount
+
+  def initialize(transferer, transferee, amount)
+    self.transferee = transferee
+    self.transferer = transferer
+    self.amount = amount
+  end
 end
